c8c2bb08037af6afd3e3062e9e9d8b97d4629c37
David <dxvidnguyen@gmail.com>
2023-12-08 16:39:37 -0800

    first commit

---
+++ finances-app/finances_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  finances_appApp.swift
+//  finances-app
+//
+//  Created by David Nguyen on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct finances_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
